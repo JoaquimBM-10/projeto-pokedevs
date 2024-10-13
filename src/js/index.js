050d852e3c0c43746405c03b60a0460df46ba60e
JoaquimBM-10 <joaquimbm2010@gmail.com>
2024-10-12 22:56:32 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+const listaSelecaoPokedevs = document.querySelectorAll(".pokedev");
+
+listaSelecaoPokedevs.forEach(pokedev => {
+    pokedev.addEventListener("click", () => {
+        esconderCartaoPokedev();
+        const idPokedevSelecionado = mostrarCartaoPokedevSelecionado(pokedev);
+        desativarPokedevNaListagem();
+        ativarPokedevSelecionadoNaListagem(idPokedevSelecionado);
+    })
+})
+
+function ativarPokedevSelecionadoNaListagem(idPokedevSelecionado) {
+    const pokedevSelecionadoNaListagem = document.getElementById(idPokedevSelecionado);
+    pokedevSelecionadoNaListagem.classList.add("ativo");
+}
+
+function desativarPokedevNaListagem() {
+    const pokedevAtivoNaListagem = document.querySelector(".ativo");
+    pokedevAtivoNaListagem.classList.remove("ativo");
+}
+
+function mostrarCartaoPokedevSelecionado(pokedev) {
+    const idPokedevSelecionado = pokedev.attributes.id.value;
+    const idDoCataoPokedevParaAbrir = "cartao-" + idPokedevSelecionado;
+    const cartaoPokedevParaAbrir = document.getElementById(idDoCataoPokedevParaAbrir);
+    cartaoPokedevParaAbrir.classList.add("aberto");
+    return idPokedevSelecionado;
+}
+
+function esconderCartaoPokedev() {
+    const cartaoPokedevAberto = document.querySelector(".aberto");
+    cartaoPokedevAberto.classList.remove("aberto");
+}
